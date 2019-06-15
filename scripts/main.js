85ecadfbc74b51f14c6f0b72fae0a1412e46c02c
Pavel Ten <pavelten@Pavels-MacBook-Air.local>
2019-06-15 10:01:49 +0300

    js change photo! :)

---
+++ scripts/main.js
@@ -0,0 +1,12 @@
+var DETAIL_IMAGE_SELECTOR = '[data-image-role="target"]';
+var DETAIL_TITLE_SELECTOR = '[data-image-role="title"]';
+var THUMBNAIL_LINK_SELECTOR = '[data-image-role="trigger"]';
+
+function setDetails() {
+    'use strict';
+    var detailImage = document.querySelector(DETAIL_IMAGE_SELECTOR);
+    detailImage.setAttribute('src','img/otter3.jpg');
+    
+    var detailTitle = document.querySelector(DETAIL_TITLE_SELECTOR);
+    detailTitle.textContent='You should be dancing';
+}
