2bd1cbfecbcaaa1cbda55b7c8e7dbfed069fd113
Pavel Ten <pavelten@Pavels-MacBook-Air.local>
2019-06-29 19:22:51 +0300

    js next

--- scripts/main.js
+++ scripts/main.js
@@ -2,6 +2,7 @@ var DETAIL_IMAGE_SELECTOR = '[data-image-role="target"]';
 var DETAIL_TITLE_SELECTOR = '[data-image-role="title"]';
 var THUMBNAIL_LINK_SELECTOR = '[data-image-role="trigger"]';
 var HIDDEN_DETAIL_CLASS = 'hidden-detail';
+var ESC_KEY = 27;
 
 function setDetails(imageUrl, titleText) {
     'use strict';
@@ -36,6 +37,7 @@ function addThumbClickHandler(thumb){
     thumb.addEventListener('click',function(event){
         event.preventDefault();
         setDetailsFromThumb(thumb);
+        showDetails();
     })
 }
 
@@ -51,11 +53,19 @@ function hideDetails() {
     document.body.classList.add(HIDDEN_DETAIL_CLASS);
 }
 
+function showDetails() {
+    'use strict';
+    document.body.classList.remove(HIDDEN_DETAIL_CLASS);
+}
+
 function addKeyPressHandler() {
     'use strict';
     document.body.addEventListener('keyup', function (event){
         event.preventDefault();
         console.log(event.keyCode);
+        if (event.keyCode === ESC_KEY) {
+            hideDetails();
+        }
     })
 }
 
